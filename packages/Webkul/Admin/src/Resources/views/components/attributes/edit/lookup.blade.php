5629cb266aafb649062c2d53115348404886e27c
Noronha <thiagonoronha@outlook.com>
2026-02-02 20:57:43 -0300

    feat: Add a new lookup attribute component for editing attribute values in the admin panel.

--- packages/Webkul/Admin/src/Resources/views/components/attributes/edit/lookup.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/attributes/edit/lookup.blade.php
@@ -3,17 +3,13 @@
         $lookUpEntityData = app('Webkul\Attribute\Repositories\AttributeRepository')->getLookUpEntity($attribute->lookup_type, old($attribute->code) ?: $value);
     @endphp
 
-    <v-lookup-component
-        :attribute="{{ json_encode($attribute) }}"
-        :validations="'{{ $validations }}'"
-        :value="{{ json_encode($lookUpEntityData)}}"
-        can-add-new="{{ $canAddNew ?? false }}"
-        @lookup-added="handleLookupAdded"
-        @lookup-removed="handleLookupRemoved"
-    >
+    <v-lookup-component :attribute="{{ json_encode($attribute) }}" :validations="'{{ $validations }}'"
+        :value="{{ json_encode($lookUpEntityData)}}" can-add-new="{{ $canAddNew ?? false }}"
+        @lookup-added="handleLookupAdded" @lookup-removed="handleLookupRemoved">
         <div class="relative inline-block w-full">
             <!-- Input Container -->
-            <div class="relative flex items-center justify-between rounded border border-gray-200 p-2 hover:border-gray-400 focus:border-gray-400 dark:border-gray-800 dark:text-gray-300">
+            <div
+                class="relative flex items-center justify-between rounded border border-gray-200 p-2 hover:border-gray-400 focus:border-gray-400 dark:border-gray-800 dark:text-gray-300">
                 @lang('admin::app.components.attributes.lookup.click-to-add')
 
                 <!-- Icons Container -->
@@ -27,126 +23,123 @@
 @endif
 
 @pushOnce('scripts')
-    <script
-        type="text/x-template"
-        id="v-lookup-component-template"
-    >
-        <div
-            class="relative"
-            ref="lookup"
-        >
+    <script type="text/x-template" id="v-lookup-component-template">
             <div
-                class="relative inline-block w-full"
-                @click="toggle"
+                class="relative"
+                ref="lookup"
             >
-                <!-- Input Container -->
                 <div
-                    class="relative flex items-center justify-between rounded border border-gray-200 p-2 hover:border-gray-400 focus:border-gray-400 dark:border-gray-800 dark:text-gray-300"
-                    :class="{
-                        'bg-gray-50': isDisabled,
-                    }"
+                    class="relative inline-block w-full"
+                    @click="toggle"
                 >
-                    <!-- Selected Item or Placeholder Text -->
-                    <span
-                        class="overflow-hidden text-ellipsis"
-                        :title="selectedItem?.name"
+                    <!-- Input Container -->
+                    <div
+                        class="relative flex items-center justify-between rounded border border-gray-200 p-2 hover:border-gray-400 focus:border-gray-400 dark:border-gray-800 dark:text-gray-300"
+                        :class="{
+                            'bg-gray-50': isDisabled,
+                        }"
                     >
-                        @{{ selectedItem?.name !== "" ? selectedItem?.name : "@lang('admin::app.components.attributes.lookup.click-to-add')" }}
-                    </span>
+                        <!-- Selected Item or Placeholder Text -->
+                        <span
+                            class="overflow-hidden text-ellipsis"
+                            :title="selectedItem?.name"
+                        >
+                            @{{ selectedItem?.name !== "" ? selectedItem?.name : "@lang('admin::app.components.attributes.lookup.click-to-add')" }}
+                        </span>
 
-                    <!-- Icons Container -->
-                    <div class="flex items-center gap-2">
-                        <!-- Close Icon -->
-                        <i
-                            v-if="
-                                ! isDisabled
-                                && (
-                                    selectedItem?.name
-                                    && ! isSearching
-                                )"
-                            class="icon-cross-large cursor-pointer text-2xl text-gray-600"
-                            @click="remove"
-                        ></i>
+                        <!-- Icons Container -->
+                        <div class="flex items-center gap-2">
+                            <!-- Close Icon -->
+                            <i
+                                v-if="
+                                    ! isDisabled
+                                    && (
+                                        selectedItem?.name
+                                        && ! isSearching
+                                    )"
+                                class="icon-cross-large cursor-pointer text-2xl text-gray-600"
+                                @click="remove"
+                            ></i>
 
-                        <!-- Arrow Icon -->
-                        <i
-                            class="text-2xl text-gray-600"
-                            :class="showPopup ? 'icon-up-arrow' : 'icon-down-arrow'"
-                        ></i>
+                            <!-- Arrow Icon -->
+                            <i
+                                class="text-2xl text-gray-600"
+                                :class="showPopup ? 'icon-up-arrow' : 'icon-down-arrow'"
+                            ></i>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <!-- Hidden Input Entity Value -->
-            <x-admin::form.control-group.control
-                type="hidden"
-                ::name="attribute['code']"
-                v-model="selectedItem.id"
-                ::rules="validations"
-                ::label="attribute['name']"
-            />
+                <!-- Hidden Input Entity Value -->
+                <x-admin::form.control-group.control
+                    type="hidden"
+                    ::name="attribute['code']"
+                    v-model="selectedItem.id"
+                    ::rules="validations"
+                    ::label="attribute['name']"
+                />
 
-            <!-- Popup Box -->
-            <div
-                v-if="showPopup"
-                class="absolute top-full z-10 mt-1 flex w-full origin-top transform flex-col gap-2 rounded-lg border border-gray-200 bg-white p-2 shadow-lg transition-transform dark:border-gray-900 dark:bg-gray-800"
-            >
-                <!-- Search Bar -->
-                <div class="relative flex items-center">
-                    <!-- Input Box -->
-                    <input
-                        type="text"
-                        v-model.lazy="searchTerm"
-                        v-debounce="500"
-                        class="w-full rounded border border-gray-200 px-2.5 py-2 text-sm font-normal text-gray-800 transition-all hover:border-gray-400 focus:border-gray-400 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-gray-400 dark:focus:border-gray-400"
-                        placeholder="@lang('admin::app.components.attributes.lookup.search')"
-                        ref="searchInput"
-                        @keyup="search"
-                    />
+                <!-- Popup Box -->
+                <div
+                    v-if="showPopup"
+                    class="absolute top-full z-10 mt-1 flex w-full origin-top transform flex-col gap-2 rounded-lg border border-gray-200 bg-white p-2 shadow-lg transition-transform dark:border-gray-900 dark:bg-gray-800"
+                >
+                    <!-- Search Bar -->
+                    <div class="relative flex items-center">
+                        <!-- Input Box -->
+                        <input
+                            type="text"
+                            v-model.lazy="searchTerm"
+                            v-debounce="500"
+                            class="w-full rounded border border-gray-200 px-2.5 py-2 text-sm font-normal text-gray-800 transition-all hover:border-gray-400 focus:border-gray-400 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-300 dark:hover:border-gray-400 dark:focus:border-gray-400"
+                            placeholder="@lang('admin::app.components.attributes.lookup.search')"
+                            ref="searchInput"
+                            @keyup="search"
+                        />
 
-                    <!-- Search Icon (absolute positioned) -->
-                    <span class="absolute flex items-center ltr:right-2 rtl:left-2">
-                        <!-- Loader (optional, based on condition) -->
-                        <div
-                            class="relative"
-                            v-if="isSearching"
-                        >
-                            <x-admin::spinner />
-                        </div>
-                    </span>
-                </div>
+                        <!-- Search Icon (absolute positioned) -->
+                        <span class="absolute flex items-center ltr:right-2 rtl:left-2">
+                            <!-- Loader (optional, based on condition) -->
+                            <div
+                                class="relative"
+                                v-if="isSearching"
+                            >
+                                <x-admin::spinner />
+                            </div>
+                        </span>
+                    </div>
 
-                <!-- Results List -->
-                <ul class="max-h-40 divide-y divide-gray-100 overflow-y-auto">
-                    <li
-                        v-for="item in filteredResults"
-                        :key="item.id"
-                        class="flex cursor-pointer gap-2 p-2 transition-colors hover:bg-blue-100 dark:text-gray-300 dark:hover:bg-gray-900"
-                        @click="handleResult(item)"
-                    >
-                        <!-- Entity Name -->
-                        <span>@{{ item.name }}</span>
-                    </li>
-
-                    <template v-if="filteredResults.length === 0">
-                        <li class="px-4 py-2 text-center text-gray-500">
-                            @lang('admin::app.components.attributes.lookup.no-result-found')
-                        </li>
-
+                    <!-- Results List -->
+                    <ul class="max-h-40 divide-y divide-gray-100 overflow-y-auto">
                         <li
-                            v-if="searchTerm.length > 2 && canAddNew"
-                            @click="handleResult({ id: '', name: searchTerm })"
-                            class="cursor-pointer border-t border-gray-800 px-4 py-2 text-gray-500 hover:bg-brandColor hover:text-white dark:border-gray-300 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-white"
+                            v-for="item in filteredResults"
+                            :key="item.id"
+                            class="flex cursor-pointer gap-2 p-2 transition-colors hover:bg-blue-100 dark:text-gray-300 dark:hover:bg-gray-900"
+                            @click="handleResult(item)"
                         >
-                            <i class="icon-add text-md"></i>
-
-                            @lang('admin::app.components.lookup.add-as-new')
+                            <!-- Entity Name -->
+                            <span>@{{ item.name }}</span>
                         </li>
-                    </template>
-                </ul>
+
+                        <template v-if="filteredResults.length === 0">
+                            <li class="px-4 py-2 text-center text-gray-500">
+                                @lang('admin::app.components.attributes.lookup.no-result-found')
+                            </li>
+
+                            <li
+                                v-if="searchTerm.length > 2 && canAddNew"
+                                @click="handleResult({ id: '', name: searchTerm })"
+                                class="cursor-pointer border-t border-gray-800 px-4 py-2 text-gray-500 hover:bg-brandColor hover:text-white dark:border-gray-300 dark:text-gray-400 dark:hover:bg-gray-900 dark:hover:text-white"
+                            >
+                                <i class="icon-add text-md"></i>
+
+                                @lang('admin::app.components.lookup.add-as-new')
+                            </li>
+                        </template>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </script>
+        </script>
 
     <script type="module">
         app.component('v-lookup-component', {
@@ -212,39 +205,35 @@
                         return;
                     }
 
-                    this.showPopup = ! this.showPopup;
+                    this.showPopup = !this.showPopup;
 
                     if (this.showPopup) {
-                        this.$nextTick(() => this.$refs.searchInput.focus());
+                        this.$nextTick(() => {
+                            this.$refs.searchInput.focus();
+
+                            this.search();
+                        });
                     }
                 },
 
                 search() {
-                    if (this.searchTerm.length <= 2) {
-                        this.searchedResults = [];
-
-                        this.isSearching = false;
-
-                        return;
-                    }
-
                     this.isSearching = true;
 
                     this.$axios.get(this.searchRoute, {
-                            params: { query: this.searchTerm }
-                        })
-                        .then (response => {
+                        params: { query: this.searchTerm }
+                    })
+                        .then(response => {
                             this.searchedResults = response.data;
                         })
-                        .catch (error => {})
+                        .catch(error => { })
                         .finally(() => this.isSearching = false);
                 },
 
                 getLookUpEntity() {
                     this.$axios.get(this.lookupEntityRoute, {
-                            params: { query: this.value?.id ?? ""}
-                        })
-                        .then (response => {
+                        params: { query: this.value?.id ?? "" }
+                    })
+                        .then(response => {
                             this.selectedItem = Object.keys(response.data).length
                                 ? response.data
                                 : {
@@ -252,7 +241,7 @@
                                     name: ''
                                 };
                         })
-                        .catch (error => {});
+                        .catch(error => { });
                 },
 
                 handleResult(result) {
@@ -270,7 +259,7 @@
 
                     if (
                         lookup &&
-                        ! lookup.contains(event.target)
+                        !lookup.contains(event.target)
                     ) {
                         this.showPopup = false;
                     }
