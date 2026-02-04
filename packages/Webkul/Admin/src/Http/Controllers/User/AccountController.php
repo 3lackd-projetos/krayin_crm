06ff0cdb38f9d418a8a1853c1c0d84b4d03869c3
Noronha <thiagonoronha@outlook.com>
2026-02-04 13:07:29 -0300

    feat: implement admin user account profile editing and updating functionality.

--- packages/Webkul/Admin/src/Http/Controllers/User/AccountController.php
+++ packages/Webkul/Admin/src/Http/Controllers/User/AccountController.php
@@ -31,11 +31,11 @@ class AccountController extends Controller
         $user = auth()->guard('user')->user();
 
         $this->validate(request(), [
-            'name'             => 'required',
-            'email'            => 'email|unique:users,email,'.$user->id,
-            'password'         => 'nullable|min:6|confirmed',
+            'name' => 'required',
+            'email' => 'email|unique:users,email,' . $user->id,
+            'password' => 'nullable|min:6|confirmed',
             'current_password' => 'required|min:6',
-            'image.*'          => 'nullable|mimes:bmp,jpeg,jpg,png,webp',
+            'image.*' => 'nullable|mimes:bmp,jpeg,jpg,png,webp',
         ]);
 
         $data = request()->only([
@@ -47,7 +47,7 @@ class AccountController extends Controller
             'image',
         ]);
 
-        if (! Hash::check($data['current_password'], $user->password)) {
+        if (!Hash::check($data['current_password'], $user->password)) {
             session()->flash('warning', trans('admin::app.account.edit.invalid-password'));
 
             return redirect()->back();
@@ -61,7 +61,7 @@ class AccountController extends Controller
 
         $isPasswordChanged = false;
 
-        if (! $data['password']) {
+        if (!$data['password']) {
             unset($data['password']);
         } else {
             $isPasswordChanged = true;
@@ -70,17 +70,9 @@ class AccountController extends Controller
         }
 
         if (request()->hasFile('image')) {
-            $data['image'] = current(request()->file('image'))->store('admins/'.$user->id);
+            $data['image'] = current(request()->file('image'))->store('admins/' . $user->id);
         } else {
-            if (! isset($data['image'])) {
-                if (! empty($data['image'])) {
-                    Storage::delete($user->image);
-                }
-
-                $data['image'] = null;
-            } else {
-                $data['image'] = $user->image;
-            }
+            $data['image'] = $user->image;
         }
 
         $user->update($data);
